366239ab9285b2857c14d4f0a02e3e5758a6a81c
Maxime Zarka <maxime.zarka@gmail.com>
2018-11-23 11:17:13 +0100

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'cocktails#index'
   get 'doses/new'
   get 'doses/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
